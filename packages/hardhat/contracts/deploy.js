eae64c9628423205781363883b1510075aa3421b
Nick Ulven <nulven@mit.edu>
2021-01-24 14:11:59 -0500

    Fix deploy script

--- packages/hardhat/contracts/deploy.js
+++ packages/hardhat/contracts/deploy.js
@@ -1,6 +1,7 @@
 const solc = require('solc');
 const linker = require('solc/linker');
 const Contract = require('web3-eth-contract');
+const web3 = require('web3');
 const fs = require('fs');
 
 const messageValidatorFile = fs.readFileSync('./MessageValidator.sol').toString();
@@ -56,38 +57,20 @@ const verifierBytecode = output1.contracts['Verifier.sol']['Verifier'].evm.bytec
 const verifierAbi = output1.contracts['Verifier.sol']['Verifier'].abi;
 const pairingBytecode = output1.contracts['Verifier.sol']['Pairing'].evm.bytecode.object;
 const pairingAbi = output1.contracts['Verifier.sol']['Pairing'].abi;
-const verifierContract = new Contract(verifierAbi, '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe');
+const verifierContract = new Contract(verifierAbi, '0x587B3c7D9E252eFFB9C857eF4c936e2072b741a4');
 verifierContract.deploy({ data: verifierBytecode });
-const pairingContract = new Contract(pairingAbi, '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAf');
+const pairingContract = new Contract(pairingAbi, '0xC7367381394E7964156d9e8C236c1E1feB06AF1d');
 pairingContract.deploy({ data: pairingBytecode });
-console.log(pairingContract.options.address);
 
 const output = JSON.parse(
   solc.compile(JSON.stringify(input), { import: findImports })
 );
+const messageValidatorAbi = output.contracts['MessageValidator.sol']['MessageValidator'].abi;
 var messageValidatorBytecode = output.contracts['MessageValidator.sol']['MessageValidator'].evm.bytecode.object;
 
-messageValidatorBytecode = linker.linkBytecode(messageValidatorBytecode, { Verifier: '', Pairing: '[LIBRARY_ADDRESS]' });
+messageValidatorBytecode = linker.linkBytecode(messageValidatorBytecode, { Verifier: verifierContract.options.address, Pairing: pairingContract.options.address });
 
-//console.log(bytecode);
-//const contract = new Contract(bytecode);
-//contract.deploy({ data: bytecode });
-/*
-for (var contractName in output.contracts['Verifier.sol']) {
-  console.log(
-    contractName +
-      ': ' +
-      output.contracts['Verifier.sol'][contractName].evm.bytecode.object
-  );
-}
-const contract1 = new Contract(bytecode);
-
-for (var contractName in output.contracts['MessageValidator.sol']) {
-  console.log(
-    contractName +
-      ': ' +
-      output.contracts['MessageValidator.sol'][contractName].evm.bytecode.object
-  );
-}
-*/
-
+// if you need to add parameters to the constructor
+//messageValidatorBytecode = messageValidatorBytecode + web3.eth.abi.encodeParameters([address...], ['0x...']);
+const contract = new Contract(messageValidatorAbi);
+contract.deploy({ data: messageValidatorBytecode });
